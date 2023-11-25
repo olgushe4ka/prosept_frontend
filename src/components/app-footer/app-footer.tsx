7c68158030c3477c347d399f8bb0f75093267db3
Olga <olgushe4ka@gmail.com>
2023-11-25 12:34:28 +0400

    hover on btms

--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -1,4 +1,3 @@
-import { FaMailBulk, FaPhone, FaTelegram, FaWhatsapp } from "react-icons/fa";
 import styles from "./app-footer.module.css";
 
 function AppFooter() {
@@ -7,30 +6,7 @@ function AppFooter() {
       {" "}
       <div className={styles.footerContainer}>
         <p className={`${styles.text} `}>2023 год</p>
-        {/* <div className={styles.contactBox}>
-          <a className={styles.contactpageContacts} href="tel:+79033883601">
-            <FaPhone /> +7 903 388 36 01
-          </a>
-          <a
-            className={styles.contactpageContacts}
-            href="https://wa.me/79033883601?text=Я%20хочу%20в%20узнать%20про%20дизайн%20интерьера"
-          >
-            <FaWhatsapp /> WhatsApp
-          </a>
-          <a
-            className={styles.contactpageContacts}
-            href="https://t.me/LJinterior"
-          >
-            <FaTelegram /> Telegram
-          </a>
-
-          <a
-            className={styles.contactpageContacts}
-            href="mailto:ljinterior@mail.ru"
-          >
-            <FaMailBulk /> E-mail
-          </a>
-        </div> */}
+        
       </div>
     </footer>
   );
